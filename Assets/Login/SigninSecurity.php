5b8ee88e980450cf75e0a763933ab1f6165a1575
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-06 08:24:36 +0800

    update here

--- Assets/Login/SigninSecurity.php
+++ Assets/Login/SigninSecurity.php
@@ -38,15 +38,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/Assets/Login/login.css">
-  <!-- This is Bootstrap CDN -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <link rel="icon" type="img/x-icon" href="#">
-  <title>Login</title>
-</head>
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="/Assets/Login/login.css">
+      <!-- This is Bootstrap CDN -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+      <link rel="icon" type="img/x-icon" href="#">
+      <title>Login</title>
+  </head>
 <body>
 
   <section class="form-parent">
@@ -71,6 +71,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <!-- This is CDN Bootstrap -->
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  
 </body>
 </html>
