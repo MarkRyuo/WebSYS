b3b09cea819cb7c8e37186032327605bece3a529
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-06 09:01:13 +0800

    update here

--- Assets/Profile_admin/userProfile.php
+++ Assets/Profile_admin/userProfile.php
@@ -33,32 +33,19 @@ $usersign = $_SESSION['usersign'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link rel="icon" type="img/x-icon" href="/Assets/Images/Batstatelogo.png">
-    <link rel="stylesheet" href="/Assets/Aside/Aside.css">
-    <link rel="stylesheet" href="/User-Profile/Userprofile.css">
-    <link rel="stylesheet" href="/assets/css/btn-aboutandlogout.css">
-    <link rel="stylesheet" href="/stylecursor.css">
-    <title>User Profile | Lost and Found</title>
+    <link rel="stylesheet" href="/Assets/Profile_admin/Userprofile.css">
+    <title>User Profile | Admin</title>
 </head>
 <body>
 
   <main class="main-User">
-    <!-- header here -->
-    <header>
-      <nav class="parent-Nav">
-        <h1><span><i class="fa-solid fa-user-shield fa-beat-fade"></i></span>User Profile</h1>
-      </nav>
-    </header>
-
     <!--Section Here  -->
     <section class="parent-User">
       
         <div class="user-button-Log-out">
-          <button id="Logout" class="box btn-color-Logout">
-            <i class="fa-solid fa-right-from-bracket fa-bounce"></i> <!--This is Bouncing Icon-->
-            Logout  <!--This is Text Logout-->
-          </button> 
+          <button class="button2" id="Back">
+             Back
+          </button>  
         </div>
         
       <form class="input" >
@@ -76,7 +63,5 @@ $usersign = $_SESSION['usersign'];
 
   </main>
 </body>
-  <!-- Connection in Home.js -->
-  <script src="/assets/js/Logout.js"></script>
-  <script src="/Assets/Aside/btn-aside.js"></script>
+  <!-- Connection -->
 </html>
