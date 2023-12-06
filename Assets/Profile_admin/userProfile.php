5e7e5078783a202cdd11e1223787779a503017c1
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-06 09:01:45 +0800

    update here

--- Assets/Profile_admin/userProfile.php
+++ Assets/Profile_admin/userProfile.php
@@ -64,4 +64,15 @@ $usersign = $_SESSION['usersign'];
   </main>
 </body>
   <!-- Connection -->
+  <script>
+      // This is Btn Logout 
+
+  var Back = document.getElementById("Back");
+
+  Back.addEventListener("click", function() {
+    // Redirect to "/User-Profile/userProfile.php" directly
+    window.location.href = "/Assets/Staff_Dashboard/Dashboard.html";
+  });
+  </script>
+  
 </html>
